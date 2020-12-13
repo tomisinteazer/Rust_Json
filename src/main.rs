e7f22685145bea920c534f31f5823be31f0efcc9
tomisinteazer <tomisinteazer@gmail.com>
2020-12-13 05:21:02 +0100

    deserializer removed redundant comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[allow(non_snake_case)]
 use serde::{Deserialize, Serialize};
-use std::fmt;
 use std::fs::read_to_string;
+
+
+
+
 #[derive(Serialize, Deserialize)]
 struct page_info {
     totalResults: u16,
@@ -50,24 +54,16 @@ struct item_data {
     id: id_data,
     snippet: snippet_data,
 }
+
+
+
+
+
 fn main() {
-    // Some JSON input data as a &str. Maybe this comes from the user.
     let data = read_to_string("recipes.json").unwrap();
-
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
     let recipes_json: res_data = serde_json::from_str(&data).unwrap();
-
-    // Do things just like with any other Rust data structure.
     println!(
         "Please call {} at the number {:#?}",
         recipes_json.kind, recipes_json.items[0].snippet.title
     );
 }
-
-// impl fmt::Debug for item_data {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "Hi")
-//     }
-// }
